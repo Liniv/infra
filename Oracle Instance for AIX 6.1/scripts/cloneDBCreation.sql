9c2368da6dd0e0b1428a64ea12120662c792f837
Aleh Sinitsa <asinitsa@iba.by>
2019-02-22 11:30:36 +0300

    Upload New File

---
+++ Oracle Instance for AIX 6.1/scripts/cloneDBCreation.sql
@@ -0,0 +1,48 @@
+SET VERIFY OFF
+connect "SYS"/"&&sysPassword" as SYSDBA
+set echo on
+spool /oracle/admin/IBAPP/scripts/cloneDBCreation.log append
+Create controlfile reuse set database "IBAPP"
+MAXINSTANCES 8
+MAXLOGHISTORY 1
+MAXLOGFILES 16
+MAXLOGMEMBERS 3
+MAXDATAFILES 100
+Datafile 
+'/webapp/IBAPP/system01.dbf',
+'/webapp/IBAPP/sysaux01.dbf',
+'/webapp/IBAPP/undotbs01.dbf',
+'/webapp/IBAPP/users01.dbf'
+LOGFILE GROUP 1 ('/webapp/IBAPP/redo01.log') SIZE 51200K,
+GROUP 2 ('/webapp/IBAPP/redo02.log') SIZE 51200K,
+GROUP 3 ('/webapp/IBAPP/redo03.log') SIZE 51200K RESETLOGS;
+exec dbms_backup_restore.zerodbid(0);
+shutdown immediate;
+startup nomount pfile="/oracle/admin/IBAPP/scripts/initIBAPPTemp.ora";
+Create controlfile reuse set database "IBAPP"
+MAXINSTANCES 8
+MAXLOGHISTORY 1
+MAXLOGFILES 16
+MAXLOGMEMBERS 3
+MAXDATAFILES 100
+Datafile 
+'/webapp/IBAPP/system01.dbf',
+'/webapp/IBAPP/sysaux01.dbf',
+'/webapp/IBAPP/undotbs01.dbf',
+'/webapp/IBAPP/users01.dbf'
+LOGFILE GROUP 1 ('/webapp/IBAPP/redo01.log') SIZE 51200K,
+GROUP 2 ('/webapp/IBAPP/redo02.log') SIZE 51200K,
+GROUP 3 ('/webapp/IBAPP/redo03.log') SIZE 51200K RESETLOGS;
+alter system enable restricted session;
+alter database "IBAPP" open resetlogs;
+exec dbms_service.delete_service('seeddata');
+exec dbms_service.delete_service('seeddataXDB');
+alter database rename global_name to "IBAPP";
+ALTER TABLESPACE TEMP ADD TEMPFILE '/webapp/IBAPP/temp01.dbf' SIZE 20480K REUSE AUTOEXTEND ON NEXT 640K MAXSIZE UNLIMITED;
+select tablespace_name from dba_tablespaces where tablespace_name='USERS';
+select sid, program, serial#, username from v$session;
+alter database character set INTERNAL_CONVERT AL32UTF8;
+alter database national character set INTERNAL_CONVERT AL16UTF16;
+alter user sys account unlock identified by "&&sysPassword";
+alter user system account unlock identified by "&&systemPassword";
+alter system disable restricted session;
