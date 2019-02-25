5544c27bd50969d03ef39bdfc0af28bb60154496
Aleh Sinitsa <asinitsa@iba.by>
2019-02-25 20:42:13 +0300

    rem home_1

--- Oracle Instance for AIX 6.1/scripts/IBRAD/IBRAD.sh
+++ Oracle Instance for AIX 6.1/scripts/IBRAD/IBRAD.sh
@@ -9,9 +9,9 @@ mkdir -p /oracle/cfgtoollogs/dbca/IBRAD
 mkdir -p /radius/oradata/fast_recovery_area
 mkdir -p /radius/oradata/fast_recovery_area/IBRAD
 mkdir -p /radius/oradata/IBRAD
-mkdir -p /oracle/product/11.2.0/dbhome_1/dbs
+mkdir -p /oracle/product/11.2.0.4/dbs
 umask ${OLD_UMASK}
 ORACLE_SID=IBRAD; export ORACLE_SID
 PATH=$ORACLE_HOME/bin:$PATH; export PATH
-echo You should Add this entry in the /etc/oratab: IBRAD:/oracle/product/11.2.0/dbhome_1:Y
-/oracle/product/11.2.0/dbhome_1/bin/sqlplus /nolog @/oracle/admin/IBRAD/scripts/IBRAD.sql
+echo You should Add this entry in the /etc/oratab: IBRAD:/oracle/product/11.2.0.4:Y
+/oracle/product/11.2.0.4/bin/sqlplus /nolog @/oracle/admin/IBRAD/scripts/IBRAD.sql
--- Oracle Instance for AIX 6.1/scripts/IBRAD/IBRAD.sql
+++ Oracle Instance for AIX 6.1/scripts/IBRAD/IBRAD.sql
@@ -3,7 +3,7 @@ ACCEPT sysPassword CHAR PROMPT 'Enter new password for SYS: ' HIDE
 ACCEPT systemPassword CHAR PROMPT 'Enter new password for SYSTEM: ' HIDE
 ACCEPT sysmanPassword CHAR PROMPT 'Enter new password for SYSMAN: ' HIDE
 ACCEPT dbsnmpPassword CHAR PROMPT 'Enter new password for DBSNMP: ' HIDE
-host /oracle/product/11.2.0.4/dbhome_1/bin/orapwd file=/oracle/product/11.2.0.4/dbhome_1/dbs/orapwIBRAD force=y
+host /oracle/product/11.2.0.4/bin/orapwd file=/oracle/product/11.2.0.4/dbs/orapwIBRAD force=y
 @/oracle/admin/IBRAD/scripts/CloneRmanRestore.sql                     
 @/oracle/admin/IBRAD/scripts/cloneDBCreation.sql
 @/oracle/admin/IBRAD/scripts/postScripts.sql
